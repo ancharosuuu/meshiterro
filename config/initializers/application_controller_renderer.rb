ae0e4823c254b84446d8ec33a170679e811c3d95
EC2 Default User <ec2-user@ip-172-31-1-166.ec2.internal>
2020-12-22 14:20:31 +0000

    Sprockets::Rails::Helper::AssetNotFound in PostImages#index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7d9cd21c50318fced9bf59e5b6d5ed6395906e8abd8c33d78ca60bf77222e27f31f9fab76326fe779f568385918947da6904ad7f6bcd4053485f9eb0b8952821'
