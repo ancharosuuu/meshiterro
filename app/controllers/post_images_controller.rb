d92840f56827d4727ecbca1090deaf411b554538
EC2 Default User <ec2-user@ip-172-31-1-166.ec2.internal>
2020-12-24 10:27:22 +0000

    いいね機能実装まで

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,16 +9,25 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_images_path  end
+    redirect_to post_images_path
+  end
 
   def index
    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
+    def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
   end
 
   # 投稿データのストロングパラメータ
